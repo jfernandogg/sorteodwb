4cc4a10eceb8c9694dc47dcbc215261e89e453d2
Juan Fernando Gallego Gomez <juan.gallego@gmail.com>
2025-07-24 15:14:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,7 +4,7 @@ export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
   // and load the messages for the given locale.
   return {
-    locale, // Add the locale to the return object
+    locale,
     messages: (await import(`../messages/${locale}.json`)).default
   };
 });
