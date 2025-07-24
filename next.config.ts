5a7cdff71ba8417fe447238c390e3abae7c898a1
Juan Fernando Gallego Gomez <juan.gallego@gmail.com>
2025-07-24 14:41:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-const withNextIntl = require('next-intl/plugin')();
 
 const nextConfig = {
   typescript: {
@@ -29,4 +28,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
