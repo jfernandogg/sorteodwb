44313b726a2bf501c54792e72e914574a3f9077a
Juan Fernando Gallego Gomez <juan.gallego@gmail.com>
2025-07-24 15:31:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,8 @@
-import { getMessages } from 'next-intl/server';
+import { getMessages, unstable_setRequestLocale } from 'next-intl/server';
 import { NextIntlClientProvider } from 'next-intl';
 import Image from 'next/image';
 import LanguageSwitcher from '@/components/LanguageSwitcher';
 import AppFooter from '@/components/AppFooter';
-import {unstable_setRequestLocale} from 'next-intl/server';
 
 export default async function LocaleLayout({
   children,
@@ -20,20 +19,20 @@ export default async function LocaleLayout({
   const messages = await getMessages();
 
   return (
-      <NextIntlClientProvider locale={locale} messages={messages}>
-        <header className="w-full">
-          <Image
-            src="/banner.svg"
-            alt="Rifa Solidaria Living Center Banner"
-            width={680}
-            height={75}
-            className="w-full h-auto object-cover"
-            priority
-          />
-          <LanguageSwitcher />
-        </header>
-        {children}
-        <AppFooter />
-      </NextIntlClientProvider>
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      <header className="w-full">
+        <Image
+          src="/banner.svg"
+          alt="Rifa Solidaria Living Center Banner"
+          width={680}
+          height={75}
+          className="w-full h-auto object-cover"
+          priority
+        />
+        <LanguageSwitcher />
+      </header>
+      {children}
+      <AppFooter />
+    </NextIntlClientProvider>
   );
 }
